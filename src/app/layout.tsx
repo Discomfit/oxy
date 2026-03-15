9df7f1ef176bdc9640d4b50db88ac6a34e23e382
-.- <u57053709@gmail.com>
2026-03-14 22:10:29 -0400

    Added Vercel Analytics & Speed Tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,6 @@
+import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -20,6 +23,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.variable} antialiased`}>
+        <Analytics />
+        <SpeedInsights />
         {children}
       </body>
     </html>
